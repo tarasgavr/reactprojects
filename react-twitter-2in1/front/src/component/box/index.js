13ed08be77f4c948e15f72db37226f16ddc9dfe8
tarasgavr <tarasgavrilov1985@gmail.com>
2026-01-10 19:33:52 +0200

    GFHG

--- react-twitter-2in1/front/src/component/box/index.js
+++
@@ -1,7 +0,0 @@
-import "./index.css";
-
-export default function Component({ children, className, style = {} }) {
-    return <div style={style} className={`box ${className}`}>
-        { children }
-    </div>
-}
--- react-twitter-2in1/front/src/component/page/index.js
+++
@@ -1,5 +0,0 @@
-import "./index.css";
-
-export default function Component({ children }) {
-    return <div className="page">{ children }</div>
-}
