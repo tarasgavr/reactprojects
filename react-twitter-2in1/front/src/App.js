44e48564dc919275dde36d77b021daf76ba9bb2c
tarasgavr <tarasgavrilov1985@gmail.com>
2026-01-10 19:25:45 +0200

    GO

--- react-twitter-2in1/front/src/App.js
+++ react-twitter-2in1/front/src/App.js
@@ -1,14 +1,11 @@
-import "./App.css";
 import Page from "./component/page";
 import PostList from "./container/post-list";
 
 function App() {
   return (
-    <div className="App">
-      <Page>
-        <PostList></PostList>
-      </Page>
-    </div>
+    <Page>
+      <PostList></PostList>
+    </Page>
   )
 }
 
